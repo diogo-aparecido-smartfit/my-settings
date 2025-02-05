98c2603324e9bd93ff4bf8064d0f2bb37db0fa27
DiogoAMV <diogo.amv19@gmail.com>
2025-02-05 13:16:44 -0300

    chore: configure keyboard shortcuts for explorer

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        mappings = {
+          ["o"] = "open", -- Faz "o" funcionar igual ao "L"
+        },
+      },
+    },
+  }
+  
